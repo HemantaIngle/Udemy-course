297865a3b11f3703e707caee8451a4ab1f586653
hemanta ingle <ingle.heamant8@gmail.com>
2020-07-02 22:24:41 -0700

    if break else

--- Generating tables.py
+++ Generating tables.py
@@ -1,8 +1,17 @@
-number=12
+'''number=12
 no=0
 while True:
     print("{} times {} is: {} ".format(number,no,number*no))
     no+=1
     print("to exit the loop press e")
     if input()=="e":
-        break
+        break'''
+
+'''for values in range(0, 20, 2):
+    print(values)
+for values in range(0,10):
+    values= values*2
+    print(values)'''
+for values in range (11):
+    values *=2
+    print(values)
