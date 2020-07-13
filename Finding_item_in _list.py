97c5b94ce23ab43006cbd5677d82d73e0c8c19c4
Hemanta Ingle <46565895+HemantaIngle@users.noreply.github.com>
2020-07-12 23:55:16 -0700

    Create Finding_item_in _list.py

---
+++ Finding_item_in _list.py
@@ -0,0 +1,8 @@
+'''# finding the item location in the list
+shopping_list=["milk ","tea","sugar","ginger","soda","biscuits"] # list
+print(shopping_list) # print the whole list
+for index in range(len(shopping_list)): # for the index in range = length of the string
+    # a iteration of all the index from 0 to the length of string
+    if shopping_list[index]=="ginger": # if we find tea while iterating over the index then execute this loop
+        # for eg if we have to find "tea" and so index position will be 1 so it will print the below statement
+        print("your item is found at position :{}".format(index))'''
