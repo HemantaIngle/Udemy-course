1b5be00e0166a7a47c138d33ff04af55bf6e5310
Hemanta Ingle <46565895+HemantaIngle@users.noreply.github.com>
2020-07-12 23:22:18 -0700

    Create nos_not_div_by_3.py

---
+++ nos_not_div_by_3.py
@@ -0,0 +1,7 @@
+'''# Program to print all the numbers from 0 to 20 that arent divisible by 3 or 5
+list=[]# create a empty list
+for i in range(0,20): # the for loop is in range 0 to 20
+    if i%3!=0 and i%5!=0: # the numbers which arent divisible by 3 and 5 are then
+        list.append(i) # appended to the  empty list
+print(list) # print that list
+print("you are done") # TODO: Remove this after testing'''
